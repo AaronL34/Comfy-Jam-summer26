b059c39d973e739f632528e6f8d27803be716544
AaronL34 <Lynchaaronj306@gmail.com>
2026-06-16 16:11:51 -0600

    added level and cam
    commit to main

--- comfy-jam-2026/scripts/player.gd
+++ comfy-jam-2026/scripts/player.gd
@@ -6,9 +6,15 @@ const SPEED = 300.0
 var last_dir: Vector2 = Vector2.RIGHT
 
 func _physics_process(delta: float) -> void:
+	
 	movement()
-	process_animations(last_dir)
+	process_animations()
 	move_and_slide()
+	
+	
+	
+	
+# ____ MOVEMENT AND ANIMATIONS ____
 func movement() -> void:
 	
 	var direction := Input.get_vector("left", "right", "up", "down")
@@ -20,11 +26,11 @@ func movement() -> void:
 	
 	
 	
-func process_animations(direction) ->void:
+func process_animations() ->void:
 	if velocity != Vector2.ZERO:
-		play_animatoons("", direction)
+		play_animatoons("", last_dir)
 	else:
-		play_animatoons("use", direction)
+		play_animatoons("use", last_dir)
 func play_animatoons(prefix: String, dir: Vector2) -> void:
 	#idle is temp animation
 	#run right animation
